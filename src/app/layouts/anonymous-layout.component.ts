11b80ebada089c306583bf35fe011594df8c5dcd
Bogomil Krustev <bogomil@krustev.dev>
2025-06-24 16:04:42 +0300

    refactor: update styles and layout components
    - Removed the prebuilt Angular Material theme from angular.json, retaining only the custom styles.
    - Updated styles in styles.scss to use a new color palette (primary: violet, tertiary: orange).
    - Enhanced the anonymous layout component by adding a MatToolbar for better UI structure and centered content.
    - Introduced a new heading in the layout for improved visibility and user engagement.

--- src/app/layouts/anonymous-layout.component.ts
+++ src/app/layouts/anonymous-layout.component.ts
@@ -1,19 +1,32 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-anonymous-layout',
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule],
   template: `
+    <mat-toolbar class="anonymous-toolbar">
+      <span>My Application</span>
+    </mat-toolbar>
     <div class="anonymous-container">
       <router-outlet></router-outlet>
+      <div>
+        <h1>RECAPTCHA!!!!!!!!!!!!</h1>
+      </div>
     </div>
   `,
   styles: [
     `
+      @use '@angular/material' as mat;
+
       :host {
-        display: block;
-        height: 100%;
+        @include mat.toolbar-overrides(
+          (
+            container-background-color: var(--mat-sys-primary),
+            container-text-color: var(--mat-sys-on-primary),
+          )
+        );
       }
 
       .anonymous-container {
@@ -21,6 +34,13 @@ import { RouterModule } from '@angular/router';
         justify-content: center;
         align-items: center;
         height: 100%;
+        margin-top: -64px;
+      }
+
+      .anonymous-toolbar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
       }
     `,
   ],
